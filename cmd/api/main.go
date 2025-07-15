51b113f4f58a39fe30d37d5d111962d2b24cacd3
kaipov24 <kaipovich24@gmail.com>
2025-07-15 13:37:41 +0600

    Db connection

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,16 +1,31 @@
 package main
 
 import (
+	"github.com/kaipov24/social/internal/db"
 	"github.com/kaipov24/social/internal/env"
-	store2 "github.com/kaipov24/social/internal/store"
+	"github.com/kaipov24/social/internal/store"
 	"log"
 )
 
 func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
+		db: dbConfig{
+			addr:         env.GetString("DB_ADDR", "postgresql://admin:adminpassword@localhost/socialnetwork?sslmode=disable"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
+		},
 	}
-	store := store2.NewStorage(nil)
+
+	db, err := db.New(cfg.db.addr, cfg.db.maxOpenConns, cfg.db.maxIdleConns, cfg.db.maxIdleTime)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer db.Close()
+	log.Println("Db connection established")
+
+	store := store.NewStorage(db)
 
 	app := &application{
 		config: cfg,
